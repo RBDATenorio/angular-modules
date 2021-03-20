d357fe7840c9086ffd761b81883a938414960a6e
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-20 12:49:09 -0300

    adding modules and exporting them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { ModsModule } from './mods/mods.module';
+import { ViewsModule } from './views/views.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ModsModule,
+    ViewsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-home',
+  templateUrl: './elements-home.component.html',
+  styleUrls: ['./elements-home.component.css']
+})
+export class ElementsHomeComponent implements OnInit {
+
+  @Input() teste?: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
