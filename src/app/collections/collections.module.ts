2618a2e2506a25af0ec27fc60f50738acd1a839f
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-22 14:22:37 -0300

    using ng-content instead of @Input()

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
@@ -9,7 +10,8 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
   declarations: [CollectionsHomeComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
