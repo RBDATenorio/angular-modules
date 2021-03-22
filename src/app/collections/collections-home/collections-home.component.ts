7065cc9ecbb92875428c43074804315fe5f645a7
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-22 15:58:09 -0300

    adding reuseble table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: 'Ricardo', age: 30, job: 'Web Dev', employeed: true},
+    { name: 'Indi', age: 26, job: 'Analyst', employeed: true },
+    { name: 'Arjuna', age: 24, job: 'Musician', employeed: false },
+    { name: 'José', age: 45, job: 'Teacher', employeed: true }
+  ];
+
+  header = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent],
+  declarations: [CollectionsHomeComponent, TableComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
